00eeca88aecc98516336c139a4b34e369616b607
mzunohkaru <winkaisyu@gmail.com>
2024-03-10 21:01:34 +0900

    add:send message for line

---
+++ line_api/line_notification_api.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+LINE_NOTIFY_TOKEN = os.getenv("LINE_API_KEY")
+
+URL = "https://notify-api.line.me/api/notify"
+
+
+def notify_message(message: str):
+    headers = {
+        "Authorization": f"Bearer {LINE_NOTIFY_TOKEN}",
+        # フォームデータがURLエンコードされた形式で送信されることを設定
+        # この形式は、HTMLフォームがデータをサーバーに送信する際に一般的に使用されるもの
+        # キーと値のペアが&で区切られ、スペースは+または%20でエンコードされる
+        "Content-Type": "application/x-www-form-urlencoded",
+        # フォームデータが複数の部分（パート）に分割されて送信されることを設定
+        # この形式は、テキストデータだけでなくファイルや大量のデータを送信する場合に使用されるもの
+        # "Content-Type": "multipart/form-data",
+    }
+
+    data = {"message": message}
+
+    res = requests.post(URL, headers=headers, data=data)
+    print(res.status_code)
+
+
+message = "Line APIを用いた通知のテストです"
+
+if __name__ == "__main__":
+    notify_message(message)
